311d1cfc03199723c3bbfe590ab96786a8fb37c6
nerd8932 <samir.amin8932@gmail.com>
2025-02-14 19:34:51 -0400

    Minor changes

--- src/project.jsx
+++ src/project.jsx
@@ -17,15 +17,15 @@ function Project() {
                     Cometfall Press started off as a group of design students playing D&D many years ago in India. Our setting has been developed over many years, initially starting off as a homebrew world created for our table which continued to expand in scope. Now, our team is spread across the globe, and aims to bring our home-grown setting to players and DMs everywhere.
                 </p>
                 
-                <div className='flex flex-col mt-20'>
-                    <div className='relative w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4 m-auto z-20 p-2'>
+                <div className='flex flex-col mt-5 md:mt-10 lg:mt-10 xl:md-10'>
+                    <div className='relative w-3/5 md:w-1/3 lg:w-1/3 xl:w-1/4 m-auto z-20 p-2'>
                         <img src={worldtitle}/>
                     </div>
-                    <div className='relative flex flex-col sm:mt-10 md:mt-20 lg:mt-20 xl:mt-20 md:flex-row lg:flex-row xl:flex-row m-auto w-[90%] h-50 place-items-center'>
-                        <div className='w-3/4 md:2-2/3 xl:w-1/2 z-20 p-2 m-2'>
+                    <div className='place-items-center relative flex flex-col sm:mt-10 md:mt-20 lg:mt-20 xl:mt-20 md:flex-row lg:flex-row xl:flex-row m-auto w-[90%] h-50'>
+                        <div className='w-4/5 md:w-2/3 lg:w-3/5 xl:w-2/5 z-20 p-2 m-2'>
                             <img src={world}/>
                         </div>
-                        <p className='sm:w-2/3 h-auto p-2 clear-both m-2 text-left text-pretty text-dark sm:text-sm md:text-md lg:text-lg xl:text-xl font-semibold'>
+                        <p className='w-4/5 md:w-1/2 h-auto p-2 clear-both m-auto text-left text-pretty text-dark sm:text-sm md:text-md lg:text-lg xl:text-xl font-semibold'>
                             This book details the cultures and landscapes of the nation of Kasaaq, which is only a small whole of a greater world known as 'Abmarah'. Kasaaq is most well known for its large desert, but contains many diverse environments including dense jungles, floodplains, underground caves, volcanic mountains, tropical islands, and cool valleys.
                         </p>
                     </div>
