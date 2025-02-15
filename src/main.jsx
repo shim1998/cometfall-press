77237f27b9bbcd65b298ab789cd68c25bd073d4a
nerd8932 <samir.amin8932@gmail.com>
2025-02-15 12:45:39 -0400

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route, Navigate} from "react-router-dom";
 import './index.css'
 import Home from './home.jsx'
 import Layout from './layout.jsx'
@@ -10,12 +10,12 @@ createRoot(document.getElementById('root')).render(
   <HashRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/project" element={<Project/>} />
+          <Route path="project" element={<Project/>} />
           <Route
               path="*"
-              element={<Navigate to="/project" />}
+              element={<Navigate to="project" />}
           />
         </Route>
       </Routes>
-    </HashRouter>,
+  </HashRouter>
 )
