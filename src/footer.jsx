86e4df67f668e6a6e32b70818fdbc05135221055
shim1998 <98shim@gmail.com>
2025-02-16 14:33:06 +0530

    added mailto:

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './index.css'
 
 // Social Media Links JSON
 const socialLinks = {
@@ -7,7 +6,8 @@ const socialLinks = {
     instagram: "https://www.instagram.com/cometfallpress/",
     linkedIn: "https://www.linkedin.com/company/cometfallpress/",
     twitter: "https://twitter.com/CometfallPress",
-    facebook: "https://www.facebook.com/profile.php?id=61572637307560"
+    facebook: "https://www.facebook.com/profile.php?id=61572637307560",
+    discord: "https://discord.gg/6ScD8ZK7fX"
 };
 
 function Footer() {
@@ -17,13 +17,7 @@ function Footer() {
                 <p className="text-sm md:text-base">
                     © {new Date().getFullYear()} Cometfall Press. All Rights Reserved.
                 </p>
-                <nav className="mt-2">
-                    <ul className="flex justify-center space-x-4">
-                        <li><a href="#" className="hover:text-magenta">Home</a></li>
-                        <li><a href="#" className="hover:text-magenta">About</a></li>
-                        <li><a href="#" className="hover:text-magenta">Contact</a></li>
-                    </ul>
-                </nav>
+
                 {/* Social Media Icons */}
                 <div className="mt-4 flex justify-center space-x-6">
                     <a href={socialLinks.instagram} target="_blank" rel="noopener noreferrer" className="text-light hover:text-magenta">
@@ -40,6 +34,12 @@ function Footer() {
                     </a>
                     <a href={socialLinks.linkedIn} target="_blank" rel="noopener noreferrer" className="text-light hover:text-magenta">
                         <i className="fab fa-linkedin text-xl"></i>
+                    </a>
+                    <a href={socialLinks.discord} target="_blank" rel="noopener noreferrer" className="text-light hover:text-magenta">
+                        <i className="fab fa-discord text-xl"></i>
+                    </a>
+                    <a href="mailto:aaron.kumar@cometfallpress.com" className="text-light hover:text-magenta">
+                        <i className="fas fa-envelope text-2xl"></i>
                     </a>
                 </div>
             </div>
