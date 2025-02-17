d8915b7a2c05b84a0452646d53100f10304705d2
nerd8932 <samir.amin8932@gmail.com>
2025-02-16 20:57:34 -0400

    updates

--- src/project.jsx
+++ src/project.jsx
@@ -6,7 +6,8 @@ import houses from './assets/WebsiteHouseBanners.webp'
 import races from './assets/WebsiteRaces.webp'
 import worldtitle from './assets/WebsiteText1.webp'
 import racesitle from './assets/WebsiteText2.webp'
-import textbg from './assets/WebsiteTextBG.webp'
+import subclasses from './assets/WebsiteSubclasses.webp'
+import subclasstitle from './assets/WebsiteText3.webp'
 import book from './assets/WebsiteBook.webp'
 import { useState, useEffect } from 'react'
 import Footer from './footer.jsx'
@@ -85,10 +86,10 @@ function Project() {
                     </div>
                 </div>
                 <div className='place-items-center relative h-auto flex flex-col m-auto mt-5 w-[100vw]'>
-                    <p className='relative mx-5 pt-0 my-auto clear-both text-left lg:text-center text-pretty font-regular w-3/4 m-auto text-dark'>
+                    <p className='relative mx-5 pt-0 my-auto clear-both text-left text-pretty font-regular w-3/4 m-auto text-dark'>
                         There are many factions both outside and within Kasaaq, and many of them feature as backgrounds for player characters and NPCs alike within this book. An example here is the Swarna Guild, based on the ancient Dravidian merchants of south India. While they are not within Kasaaq itself, they control a neighboring nation and hold a lot of foreign influence within the desert.                  
                     </p>
-                    <div className='relative flex flex-col mt-10'>
+                    <div className='relative flex flex-col mt-20'>
                         <div className='relative drop-shadow-[0_0_10px_rgba(50,0,0,0.33)] w-3/5 md:w-2/5 lg:w-1/3 xl:w-1/4 m-auto z-20 p-2'>
                             <img src={racesitle} alt='Race Title' />
                         </div>
@@ -105,21 +106,54 @@ function Project() {
                             This book will contain 13 new playable races, each culturally embedded within the history of Kasaaq and inspired from Indian mythology or existing fantasy mythos. Many of these races have unique racial feats to go along with them. These races include:
                         </p>
                         <ul className='w-3/4 mx-auto my-0 mt-10 columns-1 md:columns-2'>
+                        <li className='m-4 mt-0 break-inside-avoid-column'><b>The Omniborn</b><br/> An ancient extinct race with four arms and three different subraces.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Vedavin</b><br/> A blue-skinned race deeply attuned to magic and the gods.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Panshera</b><br/> Humanoid big-cats with three subraces: Leos, Tigros, and Pardos.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Vanara</b><br/> Humanoid monkeys known for their legendary heroism and trickery.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Ursines</b><br/> Humanoid bears with two subraces: Ursos and Pandos.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Saurians</b><br/> Humanoid lizardfolk with three subraces: Crocids, Gatorids, and Gharids.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Faiiali</b><br/> Insect-like people with six arms known for their architectural genius.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Vezaborn</b><br/> A tentacled race created by the gods to survive all extremes.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Vicchu</b><br/> Half-scorpions known for their monstrous might.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Dune Orcs</b><br/> A new subrace for Orcs who dwell within the sandy wastes.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Desert Dwarves</b><br/> A new subrace for the dwarves of the arid mountains.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Sand Elves</b><br/> A new subrace for elves who are accustomed to the harsh desert.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>The Sunless Drow</b><br/> A new surface-drow subrace for elves who thrive in the cold.</li>
+                        </ul>
+
+                        <div className='relative drop-shadow-[0_0_10px_rgba(50,0,0,0.33)] w-3/5 md:w-2/5 lg:w-1/3 xl:w-1/4 m-auto mt-15 z-20 p-2'>
+                            <img src={subclasstitle} alt='Subclass Title' />
+                        </div>
+                        <div className='place-items-center relative h-auto flex flex-col m-auto lg:flex-row w-[100%] lg:w-[100%]'>
+                            <div className='w-4/5 scale-110 md:w-2/3 lg:w-3/5 xl:w-2/5 z-20 p-2 mx-5 mt-5 '>
+                                <img className='min-w-[100%]' src={subclasses} alt='Subclass' />
+                            </div>
+                            <p className='relative w-3/4 lg:w-1/2 mx-5 h-auto p-2 clear-both mt-10 text-left text-pretty text-dark'>
+                                This book details the cultures and landscapes of the nation of Kasaaq, which is only a small whole of a greater world known as <i>Abmarah</i>. Kasaaq is most well known for its large desert, but contains many diverse environments including dense jungles, floodplains, underground caves, volcanic mountains, tropical islands, and cool valleys.
+                            </p>
+                            
+                        </div>
+                        <p className='relative m-auto my-auto mt-5 lg:mt-10 clear-both text-left text-pretty font-regular w-3/4 text-dark'>
+                            This book will contain 13 new subclasses, one for each of the main classes found in 5e (including Artificer!) These are:
+                        </p>
+                        <ul className='w-3/4 mx-auto my-0 mt-10 columns-1 md:columns-2'>
                             <li className='m-4 mt-0 break-inside-avoid-column'><b>Artificer:</b> <i>The Scrapper</i><br/> A subclass that involves equal destruction and creation.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Barbarian:</b> <i>Path of the Pehlwan</i><br/> a subclass inspired by ancient Indian wrestling.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Bard:</b> <i>The College of Hymns</i><br/> a subclass dedicated to the gods and their mantras.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Cleric:</b> <i>The Star Domain</i><br/> a subclass that worships  gods of astrology and star signs.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Druid:</b> <i>Circle of the Dunes</i><br/> a subclass that maintains the primal life of the desert.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Fighter:</b> <i>The Deadeye</i><br/> a subclass using firearms for gunslingers and sharpshooters.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Monk:</b> <i>Way of the Claw</i><br/> a subclass that uses bestial claws to destroy their enemies.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Paladin:</b> <i>Oath of Domination</i><br/> a subclass dedicated to those who seek the darkness.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Ranger:</b> <i>The Veilguard</i><br/> a subclass for those who guard the veil to the afterlife.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Rogue:</b> <i>The Thuggee</i><br/> a subclass of raw strength and thuggish mysticism.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Sorcerer:</b> <i>Terror Sorcery</i><br/> a subclass that turns fear itself into arcane power.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Warlock:</b> <i>The Rakshasa Patron</i><br/> a subclass for those that make deals with the fiends.</li>
-                            <li className='m-4 break-inside-avoid-column'><b>Wizard:</b> <i>The Heretic</i><br/> a subclass that involves turning the divine into the arcane.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Barbarian:</b> <i>Path of the Pehlwan</i><br/> A subclass inspired by ancient Indian wrestling.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Bard:</b> <i>The College of Hymns</i><br/> A subclass dedicated to the gods and their mantras.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Cleric:</b> <i>The Star Domain</i><br/> A subclass that worships  gods of astrology and star signs.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Druid:</b> <i>Circle of the Dunes</i><br/> A subclass that maintains the primal life of the desert.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Fighter:</b> <i>The Deadeye</i><br/> A subclass using firearms for gunslingers and sharpshooters.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Monk:</b> <i>Way of the Claw</i><br/> A subclass that uses bestial claws to destroy their enemies.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Paladin:</b> <i>Oath of Domination</i><br/> A subclass dedicated to those who seek the darkness.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Ranger:</b> <i>The Veilguard</i><br/> A subclass for those who guard the veil to the afterlife.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Rogue:</b> <i>The Thuggee</i><br/> A subclass of raw strength and thuggish mysticism.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Sorcerer:</b> <i>Terror Sorcery</i><br/> A subclass that turns fear itself into arcane power.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Warlock:</b> <i>The Rakshasa Patron</i><br/> A subclass for those that make deals with the fiends.</li>
+                            <li className='m-4 break-inside-avoid-column'><b>Wizard:</b> <i>The Heretic</i><br/> A subclass that involves turning the divine into the arcane.</li>
                         </ul>
                     </div>
+                    <h2 href='' className='font-semibold text-5xl m-auto mt-20'>Coming Soon on Kickstarter!</h2>
+                    <p className='m-auto mt-10'>Follow us on our socials for updates.</p>
                 </div>
                 <div className='h-[200px]'/>
             
